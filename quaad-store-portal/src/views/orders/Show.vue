17da4d4592ce784cf3eb5a382ca2ec265319c51e
Quaad <ajwood1998@hotmail.co.uk>
2025-11-20 10:25:44 +0100

    set up routing and initial show page. Retrieving the id via the url passed as a prop through the router.

---
+++ quaad-store-portal/src/views/orders/Show.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Show page</h1>
+    </div>
+</template>
+<script>
+    export default {
+        props: [
+            'id'
+        ],
+        mounted() {
+            console.log(this.id)
+        }
+    }
+</script>
